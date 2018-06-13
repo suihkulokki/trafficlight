502296393aa781ffaed070e1006cbc009db96fb5
Riku Voipio <riku.voipio@linaro.org>
2018-06-13 10:19:05 +0300

    Update process stopping selector
    - Work even if running under nice
    - Warn if provided Session ID appears incorrect

--- parseps.go
+++ parseps.go
@@ -31,19 +31,24 @@ func pickVictim(Sid int, Min int) (Process, error) {
 	output := runcmd("ps xhao pid,ppid,pgid,sid,stat")
 	scanner := bufio.NewScanner(strings.NewReader(output))
 	Plist := make([]Process, 0)
+	foundAny := false
 	var p Process
 
 	for scanner.Scan() {
+		foundAny = true
 		i, err := fmt.Sscanf(scanner.Text(), "%d %d %d %d %s", &p.pid, &p.ppid, &p.pgid, &p.sid, &p.stat)
 		if err != nil {
 			return p, err
 		}
-		if i == 5 && p.sid == Sid && p.stat == "R" {
+		if i == 5 && p.sid == Sid && strings.HasPrefix(p.stat, "R") {
 			Plist = append(Plist, p)
 		}
 	}
+	if !foundAny {
+		return p, errors.New(fmt.Sprintf("No process with Session ID: %d", Sid))
+	}
 	if len(Plist) <= Min {
-		return p, errors.New("Minimum threshold reached")
+		return p, errors.New(fmt.Sprintf("Minimum threshold %d reached", Min))
 	}
 	return Plist[rand.Intn(len(Plist))], nil
 
